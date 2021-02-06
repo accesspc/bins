b102175e35ed716a504f60fd94c840beb9d16d8b
Robertas Reiciunas <accesspc@gmail.com>
2021-02-06 09:53:33 +0000

    manjaro-setup.sh

---
+++ manjaro-setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# First update after installation
+sudo pacman -Syyu
+# Yay pkg manager
+sudo pacman -Syu yay
+
+# Reboot
+
+# General tools
+yay -Syu autoconf automake binutils conky dnsutils fakeroot gcc gsmartcontrol icedtea-web ipcalc lm_sensors m4 make networkmanager-fortisslvpn network-manager-sstp nmap patch pkgconf sstp-client tcpdump vim
+
+# To allow ASDM access:
+# edit /usr/lib/jvm/java-8-openjdk/jre/lib/security/java.security and remove MD5 from jdk.jar.disabledAlgorithms=
+
+# Dev services
+yay -Syu apache mariadb mod_fcgid mongodb-bin mongodb-tools-bin mysql-workbench php-fpm robo3t-bin
+
+# Setup mariadb
+sudo mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
+sudo mkdir -p /etc/httpd/conf/vhosts
+sudo cat <<EOF | tee /etc/httpd/conf/extra/php-fpm.conf
+DirectoryIndex index.php index.html
+<FilesMatch \.php$>
+SetHandler "proxy:unix:/run/php-fpm/php-fpm.sock|fcgi://localhost/"
+</FilesMatch>
+EOF
+
+sudo sed -s -i -r 's/#?LoadModule proxy_module /LoadModule proxy_module /g' /etc/httpd/conf/httpd.conf
+sudo sed -s -i -r 's/#?LoadModule proxy_fcgi_module /LoadModule proxy_fcgi_module /g' /etc/httpd/conf/httpd.conf
+sudo echo "Include conf/extra/php-fpm.conf" >> /etc/httpd/conf/httpd.conf
+sudo echo "Include conf/vhosts/*.conf" >> /etc/httpd/conf/httpd.conf
+
+# Dev tools and services
+yay -Syu ansible aws-cli code freerdp git inkscape jq nodejs python-pylint remmina terraform vault
+sudo pip3 install hvac
+
+# Pidgin skype plugin
+# yay -Syu gmime libpurple intltool pidgin-sipe
+
+# Extra software from AUR
+yay -Syu amazon-workspaces-bin brave-bin google-chrome mattermost-desktop-bin spotify teams zoom
+
+# Citrix Receiver
+yay -Syu icaclient
+
+# Virtualization
+yay -Syu virtualbox virtualbox-ext-oracle virtualbox-guest-iso
+sudo -gpasswd -a vboxusers robertas
+sudo cat <<EOF > /etc/udev/rules.d/60-vboxdrv.rules
+SUBSYSTEM=="usb_device", ACTION=="add", RUN+="/usr/share/virtualbox/VBoxCreateUSBNode.sh $major $minor vboxusers"
+SUBSYSTEM=="usb", ACTION=="add", ENV{DEVTYPE}=="usb_device", RUN+="/usr/share/virtualbox/VBoxCreateUSBNode.sh $major $minor vboxusers"
+EOF
+
+reboot
+
+
+# Cleanup unused files and dependencies
+yay -R $(yay -Qdtq)
+
+# Unused packages
+yay -Rcs mesa-demos xfburn thunderbird xfcs4-notes-plugin
+
+# Latest kernel
+mhwd-kernel -i linux510
+mhwd-kernel -r linux59
